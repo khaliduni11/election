12dcd39e8375bf9573117e2f6ca6b080b7bad8d0
Khalid Hussein <khaliduni11@gmail.com>
2020-11-14 23:32:50 +0300

    fixing issue

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = require("path");
 
 const Port = process.env.PORT || 5000
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -17,13 +17,13 @@ app.use(cors());
 app.use("/user/authentication", Authentication);
 app.use("/users", Users);
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     let err = new Error("Not Found");
     err.status = 404;
     next(err);
 });
 
-app.use(function (error, request, response, next) {
+app.use(function(error, request, response, next){
     return response.status(error.status || 500).json({
         message: error.message || "Oop something went wrong"
     })
@@ -33,13 +33,13 @@ app.use(function (error, request, response, next) {
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
-
+  
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}
+  }
+  
 
-
-app.listen(Port, function () {
-    console.log("it is working fine");
+app.listen(Port, function(){
+    console.log(`it is working fine ${Port}`);
 })
