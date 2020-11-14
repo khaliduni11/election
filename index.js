f7bfb56d98183bda578d1ef2a13fc3e367b24586
Khalid Hussein <khaliduni11@gmail.com>
2020-11-14 23:52:51 +0300

    fixing

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = require("path");
 
 const Port = process.env.PORT || 5000
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -17,29 +17,32 @@ app.use(cors());
 app.use("/user/authentication", Authentication);
 app.use("/users", Users);
 
-app.use(function(req, res, next) {
-    let err = new Error("Not Found");
-    err.status = 404;
-    next(err);
-});
-
-app.use(function(error, request, response, next){
-    return response.status(error.status || 500).json({
-        message: error.message || "Oop something went wrong"
-    })
-})
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
-  
 
-app.listen(Port, function(){
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+app.use(function (req, res, next) {
+    let err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+app.use(function (error, request, response, next) {
+    return response.status(error.status || 500).json({
+        message: error.message || "Oop something went wrong"
+    })
+})
+
+
+
+
+app.listen(Port, function () {
     console.log(`it is working fine ${Port}`);
 })
